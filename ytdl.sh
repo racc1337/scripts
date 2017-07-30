e4844019c4880c689c7f7ba7a1210c4580874388
racc1337 <wasserfla@yahoo.de>
2017-07-30 22:15:09 +0200

    Update and rename ss1.sh to ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -5,7 +5,7 @@ youtube-dl --extract-audio --audio-format mp3 $var
 read -p "Download more files? (y/n) " answer
 case ${answer:0:1} in
     y|Y )
-        bash ss1.sh
+        bash ytdl.sh
     ;;
     * )
         sftp -P 1234 user@10.0.0.11
